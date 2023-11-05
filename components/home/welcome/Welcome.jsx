d962040b23224e93a279ac031e5aff7dd4826f4d
Erika Perciliano <erikaperciliano.developer@gmail.com>
2023-11-05 19:21:58 +0000

    creates Welcome Component

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { useState } from 'react';
 
-import styles from './welcome.style'
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native';
+import { useRouter } from 'expo-router';
+
+import { icons, SIZES } from '../../../constants';
+import styles from './welcome.style';
 
 const Welcome = () => {
+  const router = useRouter();
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Erika</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}> 
+          <TextInput 
+            style={styles.searchInput}
+            value=''
+            onChange={() => {}}
+            placeholder='What are you looking for?'
+          />
+        </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode='contain'
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
