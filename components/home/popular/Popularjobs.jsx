30a376b1574e61cf81235de070231b8f40d60492
Erika Perciliano <erikaperciliano.developer@gmail.com>
2023-11-06 23:48:23 +0000

    add content Popularjob Component

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState }from 'react';
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
+import { useRouter } from 'expo-router';
 
 import styles from './popularjobs.style'
+import { COLORS, icons, SIZES } from '../../../constants';
+import PopularJobCard from '../../common/cards/popular/PopularJobCard';
 
 const Popularjobs = () => {
+  const router = useRouter();
+  const isLoaded = false;
+  const error = false;
+
+
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popularjobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+      
+      <View style={styles.cardsContainer}>
+        {isLoaded ? (
+          <ActivityIndicator size='large' color={COLORS.primary}/>
+        ): error ? (
+          <Text>Something went wrong</Text>
+        ): (
+          <FlatList />
+        )}
+      </View>
     </View>
   )
 }
